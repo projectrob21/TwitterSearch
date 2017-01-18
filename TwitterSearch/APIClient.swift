5c0e0e91c25eb538cdc17e479fa5e2f3967470bf
Robert Deans <robdeans@gmail.com>
2017-01-18 00:03:37 -0500

    refactors and cleans up code

--- TwitterSearch/APIClient.swift
+++ TwitterSearch/APIClient.swift
@@ -12,8 +12,10 @@ import TwitterKit
 class APIClient {
     
     class func getTwitterData(completion: @escaping ([String:Any]) -> Void) {
+       
+        // api client is a couple weeks outdated :/
         
-        guard let url = URL(string: "https://api.twitter.com/1.1/statuses/user_timeline.json?oauth_consumer_key=mhMSvHsLgdmVe7UxVY3abssFw&oauth_token=3351338813-GBcOFZKDLgsfJTQpdRQmj5a5HIEEj2EnJpgj00h&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1483483262&oauth_nonce=clgEx1&oauth_version=1.0&oauth_signature=/grGFic9e+acxbUZ85q3iJkZIfE=&screen_name=potus") else { print("trouble unwrapping url"); return }
+        guard let url = URL(string: "https://api.twitter.com/1.1/statuses/user_timeline.json?oauth_consumer_key=mhMSvHsLgdmVe7UxVY3abssFw&oauth_token=3351338813-GBcOFZKDLgsfJTQpdRQmj5a5HIEEj2EnJpgj00h&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1483483262&oauth_nonce=clgEx1&oauth_version=1.0&oauth_signature=/grGFic9e+acxbUZ85q3iJkZIfE=&screen_name=realtrumpatine") else { print("trouble unwrapping url"); return }
         
         let session = URLSession.shared
         let dataTask = session.dataTask(with: url) { (data, response, error) in
--- TwitterSearch/TwitterTableViewController.swift
+++ TwitterSearch/TwitterTableViewController.swift
@@ -9,87 +9,25 @@
 import UIKit
 import TwitterKit
 
-class TwitterTableViewController: TWTRTimelineViewController {
+class TwitterTableViewController: TWTRTimelineViewController, UISearchBarDelegate {
+
+    lazy var searchBar = UISearchBar()
     
-    
-    lazy var searchBarController = UISearchController()
-    
-//    func findusertweets() {
-//        
-//        var searchtext = /*searchBarController.searchBar.text!*/ "potus"
-//        let client = TWTRAPIClient()
-//        let dataSource = TWTRUserTimelineDataSource(screenName: searchtext, apiClient: client)
-//    
-//        print("dataSource = \(dataSource.userID)")
-//
-//        let userID = dataSource.userID
-//        
-//        
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        var userID: String?
-        
-        let client = TWTRAPIClient()
-        self.dataSource = TWTRUserTimelineDataSource(screenName: "Potus", apiClient: client)
-        
-        
-        // TODO: Base this Tweet ID on some data from elsewhere in your app
-//        TWTRAPIClient().loadTweet(withID: "631879971628183552") { (tweet, error) in
-//            
-//            print("TWEET = \(tweet?.text)")
-//            
-//            userID = tweet?.author.userID
-//            
-//            
-//            TWTRAPIClient().loadUser(withID: userID!) { (user, error) in
-//                print(user?.name)
-//                print(user?.isVerified)
-//                print(user?.userID)
-//            }
-        
-            
-            
-            //            if let tweetView = TWTRTweetView(tweet: tweet) {
-            //                tweetView.center = CGPoint(x: self.view.center.x, y: self.topLayoutGuide.length + tweetView.frame.size.height / 2);
-            //                self.view.addSubview(tweetView)
-            //            } else {
-            //                NSLog("Tweet load error: %@", error!.localizedDescription)
-            //            }
-//        }
-        
-        
-        
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        configure()
     }
     
     func configure() {
-        
-        //call on twitter api; multithread; reload tableview
-        
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
     }
     
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    // MARK: - Table view data source
-//    
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//    
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if searchBar.text != "" {
+            let name = searchBar.text!
+            let client = TWTRAPIClient()
+            self.dataSource = TWTRUserTimelineDataSource(screenName: name, apiClient: client)
+        }
+    }
 }
